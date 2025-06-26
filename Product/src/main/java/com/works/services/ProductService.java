8ff47f866714c9df4195b12fc9d3ac166248ed01
Hakan ÖZER <hakanozer02@gmail.com>
2025-06-26 13:11:22 +0300

    1-Days

---
+++ Product/src/main/java/com/works/services/ProductService.java
@@ -0,0 +1,13 @@
+package com.works.services;
+
+import com.works.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+}
